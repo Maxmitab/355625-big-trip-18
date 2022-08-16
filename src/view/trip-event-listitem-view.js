e3066d25007efa74dbe9f497880ff179fafa96b9
Maxmitab <maxpavlenko@hotmail.com>
2022-08-16 22:27:16 +0300

    Исправил ошибки Eslint

--- src/view/trip-event-listitem-view.js
+++ src/view/trip-event-listitem-view.js
@@ -1,7 +1,7 @@
 import {createElement} from '../render';
 
 const createListItemContainerTemplate = () => (
-  `<ul class="trip-events__list"></ul>`
+  '<ul class="trip-events__list"></ul>'
 );
 
 export default class ListItemContainerView {
